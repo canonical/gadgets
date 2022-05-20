a3ac7163f3e7344c92937ba94cc47a7df1f4d862
Matias Piipari <matias.piipari@gmail.com>
2022-05-20 23:55:35 +0300

    Commit to safety

--- device-tree-lib/lib/src/device_tree.dart
+++ device-tree-lib/lib/src/device_tree.dart
@@ -1,5 +1,7 @@
 import 'dart:core';
+import 'package:device_tree_lib/all.dart';
 import 'package:device_tree_lib/src/info.dart';
+import 'package:device_tree_lib/src/usb.dart';
 
 class ReportNotFoundError implements Exception {}
 
@@ -10,18 +12,27 @@ class MissingDeviceReportKeyException implements Exception {
 
 class InxiKey {
   static final String info = "Info";
+  static final String usb = "USB";
 }
 
 class DeviceTree {
   final DeviceInfo info;
+  final Array<USBDevice> usbDevices;
 
-  const DeviceTree(this.info);
+  const DeviceTree(this.info, this.usbDevices);
 
   factory DeviceTree.fromMap(Map<String, dynamic> map) {
     Map<String, dynamic>? infoMap = map[InxiKey.info];
     if (infoMap == null) {
       throw MissingDeviceReportKeyException(InxiKey.info);
     }
-    return DeviceTree(DeviceInfo.fromMap(infoMap));
+
+    Array<Map<String, dynamic>>? usbDeviceMaps = map[InxiKey.usb];
+    if (infoMap == null) {
+      throw MissingDeviceReportKeyException(InxiKey.info);
+    }
+    var devices = usbDeviceMaps.map((m) => USBDevice.fromMap(m));
+
+    return DeviceTree(DeviceInfo.fromMap(infoMap), devices);
   }
 }
