fbd09c7c4b6c0060f7e26ae813283c49523b7c43
Matias Piipari <matias.piipari@gmail.com>
2022-05-24 23:03:35 +0300

    Bluetooth tests done

--- device-tree-lib/test/device_tree_lib_test.dart
+++ device-tree-lib/test/device_tree_lib_test.dart
@@ -180,6 +180,22 @@ void main() {
       expect(bluetoothSummary.chip.device, 'Intel Wireless-AC 3168 Bluetooth');
       expect(bluetoothSummary.chip.type, 'USB');
       expect(bluetoothSummary.chip.classID, 'e001');
+
+      expect(bluetoothSummary.link.aclMTU, '1021:4');
+      expect(bluetoothSummary.link.info, '');
+      expect(bluetoothSummary.link.linkMode, 'peripheral accept');
+      expect(bluetoothSummary.link.linkPolicy, 'rswitch sniff');
+      expect(bluetoothSummary.link.scoMTU, '96:6');
+
+      expect(bluetoothSummary.service.address, '1C:4D:70:22:81:CE');
+      expect(bluetoothSummary.service.bluetoothService, 'enabled,running');
+      expect(bluetoothSummary.service.hardware, 'no');
+      expect(bluetoothSummary.service.id, 'hci0');
+      expect(bluetoothSummary.service.report, 'hciconfig');
+      expect(bluetoothSummary.service.rfkBlock, '');
+      expect(bluetoothSummary.service.rfkID, '0');
+      expect(bluetoothSummary.service.software, 'yes');
+      expect(bluetoothSummary.service.state, 'down');
     });
   });
 }
