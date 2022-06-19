ace632bfd97d42ddef4a777ade404980d0a1cf7f
Matias Piipari <matias.piipari@gmail.com>
2022-06-19 21:32:03 +0300

    Removes an abstract class that will not be needed.

--- device-tree-lib/lib/src/inxi_json_representable.dart
+++
@@ -1,9 +0,0 @@
-class MethodUnimplementedException implements Exception {
-  MethodUnimplementedException();
-}
-
-abstract class InxiJSONRepresentable {
-  static bool isRepresentation(Map<String, dynamic> map) {
-    throw MethodUnimplementedException();
-  }
-}
