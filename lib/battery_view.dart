b2470bf0db75d433f17247fd10a0a3c27647e2f4
Matias Piipari <matias.piipari@gmail.com>
2022-06-27 23:29:46 +0300

    Battery view pretty much done.

--- lib/battery_view.dart
+++ lib/battery_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:device_tree_lib/all.dart';
+import 'package:unicons/unicons.dart';
 
 class BatteryView extends StatelessWidget {
   final Battery battery;
@@ -12,35 +13,79 @@ class BatteryView extends StatelessWidget {
     return Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          children: [
-            Padding(
-                padding:
-                    const EdgeInsets.only(left: 6, top: 2, bottom: 8, right: 0),
-                child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Battery: ${battery.model}",
-                      textAlign: TextAlign.left,
-                      style: const TextStyle(
-                          fontSize: 12, fontWeight: FontWeight.bold),
-                    ))),
-            Container(
-              height: 40,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(255, 107, 212, 95),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(12.0),
-                ),
-              ),
-              child: Center(
-                  child: Text(battery.charge,
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                          color: Color.fromARGB(255, 53, 106, 47)))),
-            )
-          ],
+          children: [header(), batteryChargeBar(), footer()],
         ));
+  }
+
+  Widget header() {
+    return Padding(
+        padding: const EdgeInsets.only(left: 6, top: 2, bottom: 2, right: 0),
+        child: Align(
+            alignment: Alignment.centerLeft,
+            child: Row(children: [
+              const Icon(UniconsLine.battery_bolt),
+              const SizedBox(width: 2),
+              Padding(
+                  padding: const EdgeInsets.only(
+                      left: 0, top: 0, bottom: 3, right: 0),
+                  child: Text(
+                    battery is PeripheralBattery
+                        ? battery.model
+                        : "Computer Battery",
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                        fontSize: 13, fontWeight: FontWeight.bold),
+                  )),
+            ])));
+  }
+
+  Widget footer() {
+    return Padding(
+        padding: const EdgeInsets.only(left: 6, top: 3, bottom: 0, right: 0),
+        child: Row(children: [
+          batteryHealth(),
+          const Spacer(),
+          Text(battery.status,
+              style:
+                  const TextStyle(fontSize: 13, fontWeight: FontWeight.normal)),
+          const SizedBox(width: 6)
+        ]));
+  }
+
+  Widget batteryHealth() {
+    final battery = this.battery;
+    if (battery is MachineBattery) {
+      return Row(children: [
+        const Icon(
+          Icons.favorite,
+          color: Colors.blue,
+          size: 18,
+        ),
+        const SizedBox(width: 2),
+        Text(battery.condition,
+            style: const TextStyle(fontSize: 13, fontWeight: FontWeight.normal))
+      ]);
+    } else {
+      return Container();
+    }
+  }
+
+  Widget batteryChargeBar() {
+    return Container(
+      height: 40,
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        color: Color.fromARGB(255, 107, 212, 95),
+        borderRadius: BorderRadius.all(
+          Radius.circular(12.0),
+        ),
+      ),
+      child: Center(
+          child: Text(battery.charge,
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Color.fromARGB(255, 53, 106, 47)))),
+    );
   }
 }
--- stories/battery_stories.dart
+++ stories/battery_stories.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:inspector_gadget/battery_view.dart';
 import 'package:device_tree_lib/all.dart';
 
-final battery = PeripheralBattery(
+final nearlyFullPeripheralBattery = PeripheralBattery(
     charge: "92%",
     model: "Logitech Wireless Mouse MX Master 3",
     serial: "4082-b9-7b-a7-06",
@@ -11,4 +11,20 @@ final battery = PeripheralBattery(
     rechargeable: "yes",
     device: "hidpp_battery_1");
 
-Widget fullBattery() => BatteryView(battery: battery);
+final nearlyFullMachineBattery = MachineBattery(
+    charge: "92%",
+    model: "Sunwoda L19D4PH3",
+    serial: "13167",
+    status: "Discharging",
+    type: "Li-poly",
+    condition: "58.6/61.0 Wh (96.0%)",
+    min: 15.4,
+    cycles: 46,
+    id: "BAT0",
+    volts: 15.8);
+
+Widget nearlyFullPeripheralBatteryStory() =>
+    BatteryView(battery: nearlyFullPeripheralBattery);
+
+Widget nearlyFullMachineBatteryStory() =>
+    BatteryView(battery: nearlyFullMachineBattery);
