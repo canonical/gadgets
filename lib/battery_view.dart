f2556e43c782e6b7142032e4d9c2d786e22dfce4
Matias Piipari <matias.piipari@gmail.com>
2022-06-27 22:53:17 +0300

    Closer to complete battery view

--- lib/battery_view.dart
+++ lib/battery_view.dart
@@ -10,23 +10,37 @@ class BatteryView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Container(
-        height: 50,
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          color: Color.fromARGB(255, 107, 212, 95),
-          borderRadius: BorderRadius.all(
-            Radius.circular(12.0),
-          ),
-        ),
-        child: const Center(
-            child: Text("100 %",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    color: Color.fromARGB(255, 53, 106, 47)))),
-      ),
-    );
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Padding(
+                padding:
+                    const EdgeInsets.only(left: 6, top: 2, bottom: 8, right: 0),
+                child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Battery: ${battery.model}",
+                      textAlign: TextAlign.left,
+                      style: const TextStyle(
+                          fontSize: 12, fontWeight: FontWeight.bold),
+                    ))),
+            Container(
+              height: 40,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 107, 212, 95),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(12.0),
+                ),
+              ),
+              child: Center(
+                  child: Text(battery.charge,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Color.fromARGB(255, 53, 106, 47)))),
+            )
+          ],
+        ));
   }
 }
--- stories/battery_stories.dart
+++ stories/battery_stories.dart
@@ -4,7 +4,7 @@ import 'package:inspector_gadget/battery_view.dart';
 import 'package:device_tree_lib/all.dart';
 
 final battery = PeripheralBattery(
-    charge: "50%",
+    charge: "92%",
     model: "Logitech Wireless Mouse MX Master 3",
     serial: "4082-b9-7b-a7-06",
     status: "Discharging",
